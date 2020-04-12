20bfc15dd80b4f45cb875ee3af613fae639173f4
Drew Ross <mail.drew.ross@gmail.com>
2020-04-11 18:57:11 -0700

    Add working Home and Contact links.

--- script/index.js
+++ script/index.js
@@ -1,6 +1,18 @@
 let mainContent = document.querySelector(".main-content");
+let contactContent = document.querySelector("#contact-content");
+
 let aboutLink = document.querySelector("#contact-link");
+let homeLink = document.querySelector("#home-link");
+
+let mainContentDefaultDisplay = mainContent.style.display;
+// let contactContentDefaultDisplay = contactContent.style.display;
 
 aboutLink.addEventListener('click', function(){
     mainContent.style.display = "none";
+    contactContent.style.display = "block";
+})
+
+homeLink.addEventListener('click', function(){
+    mainContent.style.display = mainContentDefaultDisplay;
+    contactContent.style.display = "none";
 })
