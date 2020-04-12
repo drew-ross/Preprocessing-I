d52d36eaba316e3d5b522214b0f36b8926050f5c
Drew Ross <mail.drew.ross@gmail.com>
2020-04-11 19:34:30 -0700

    Update footer, contact page.

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 let mainContent = document.querySelector(".main-content");
-let contactContent = document.querySelector("#contact-content");
+let contactContent = document.querySelector(".contact-content");
 
 let aboutLink = document.querySelector("#contact-link");
 let homeLink = document.querySelector("#home-link");
